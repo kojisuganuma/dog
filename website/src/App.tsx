6ac5bb0b86dd5ebb4cefaa1fc3b584a37d5fb2c1
suganuma-koji <suganuma_koji@r.recruit.co.jp>
2021-08-07 11:48:40 +0900

    pages init

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './components/ProTip';
 
-const App: React.FC = () => {
+function Copyright() {
   return (
-    <h1>Hello React!</h1>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
